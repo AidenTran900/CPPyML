b080be16133358ae18adb4c3f4f9bc8239fa63a3
ProdigiousPersonn <itsfirepig67@gmail.com>
2025-11-02 16:24:58 -0800

    Hierarchy Organization

--- source/main.cpp
+++ source/main.cpp
@@ -1,18 +1,6 @@
-/** @file main.cpp
- * Just a simple hello world using libfmt
- */
-// The previous block is needed in every file for which you want to generate documentation
 
 #include <fmt/format.h>
-#include "Helpers/matrix.cpp"
-
-// This should be in the headers
-
-/**
- * @brief A function that does nothing but generate documentation
- * @return The answer to life, the universe and everything
- */
-int foo();
+#include "helpers/linalg.cpp"
 
 int main()
 {
@@ -46,6 +34,3 @@ int main()
 
     return 0;
 }
-
-// Implementation
-int foo() { return 42; }
