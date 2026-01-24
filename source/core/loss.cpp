fb1b25cb3198d7c9c64dff26e8160acbd143cc77
ProdigiousPersonn <itsfirepig67@gmail.com>
2026-01-23 23:44:39 -0800

    Please Build MacOS

--- source/core/loss.cpp
+++ source/core/loss.cpp
@@ -136,7 +136,6 @@ LossFunction* createLoss(LossType type)
             return new RMSELoss();
         case LossType::BCE:
             return new BCELoss();
-        default:
-            throw std::invalid_argument("Unsupported loss type.");
     }
+    throw std::invalid_argument("Unsupported loss type.");
 }
